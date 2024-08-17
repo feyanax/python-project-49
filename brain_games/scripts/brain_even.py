c5852343a59e5142064ef673cc36dd79cf811675
Egor Asaturyan <egor.asaturyan@gmail.com>
2024-08-17 19:16:09 +0300

    feat: add game engine

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,49 +1,31 @@
 from random import randint
 
-import prompt
+from brain_games.game import run_game
 
 YES = "yes"
 NO = "no"
-ERROR_MESSAGE = (
-    "'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'"
-    ".\nLet's try again, {name}!"
-)
-WIN_MESSAGE = "Congratulations, {name}!"
-CORRECT = "Correct!"
-ATTEMPTS_NUM = 3
 
 
 def isEven(num: int) -> bool:
     return num % 2 == 0
 
 
-def game_logic(name: str) -> None:
-    for attempt in range(ATTEMPTS_NUM):
-        mystery_num = randint(1, 300)
-        print(f"Question: {mystery_num}")
-        user_answer = input("Your answer: ")
-        correct_answer = YES if isEven(mystery_num) else NO
-        if user_answer == correct_answer:
-            print(CORRECT)
-        else:
-            print(
-                ERROR_MESSAGE.format(
-                    user_answer=user_answer,
-                    correct_answer=correct_answer,
-                    name=name,
-                )
-            )
-            return
-    print(WIN_MESSAGE.format(name=name))
+def game_logic():
+    guess_num = randint(1, 301)
+    correct_answer = YES if isEven(guess_num) else NO
+    print(f"Question: {guess_num}")
+    user_answer = input("Your answer: ")
+    return [correct_answer, user_answer]
 
 
 def main() -> None:
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}")
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    game_logic(name)
+    run_game(
+        rules='Answer "yes" if the number is even, otherwise answer "no".',
+        game_logic=game_logic,
+    )
 
 
+if __name__ == "__main__":
+    main()
 if __name__ == "__main__":
     main()
