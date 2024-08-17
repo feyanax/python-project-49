5e4051bb605218e2bd7f138c051299eb64910a20
Egor Asaturyan <egor.asaturyan@gmail.com>
2024-08-17 19:52:48 +0300

    feat: add brain-calc game

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,32 @@
+from random import choice, randint
+
+from brain_games.game import run_game
+
+
+def game_logic():
+    left_operand = randint(1, 100)
+    right_operand = randint(1, 100)
+    operator = choice(["+", "-", "*"])
+    print(f"Question: {left_operand}{operator}{right_operand}")
+    user_answer = input("Your answer: ")
+
+    match operator:
+        case "+":
+            correct_answer = left_operand + right_operand
+        case "-":
+            correct_answer = left_operand - right_operand
+        case "*":
+            correct_answer = left_operand * right_operand
+
+    return [user_answer, str(correct_answer)]
+
+
+def main():
+    run_game(
+        rules="What is the result of the expression?",
+        game_logic=game_logic,
+    )
+
+
+if __name__ == "__main__":
+    main()
