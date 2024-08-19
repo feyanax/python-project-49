de1c104c79b5ebd5a6d7ab607a0ad2150a913334
Egor Asaturyan <egor.asaturyan@gmail.com>
2024-08-19 16:00:24 +0300

    fix: switch camelcase to snakecase in is_prime func

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,7 +5,7 @@ from brain_games.game import NO, YES
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def isPrime(num) -> bool:
+def is_prime(num) -> bool:
     if num % 2 == 0:
         return num == 2
     d = 3
@@ -16,7 +16,7 @@ def isPrime(num) -> bool:
 
 def game_logic() -> list:
     guess_num = randint(1, 1000)
-    correct_answer = YES if isPrime(guess_num) else NO
+    correct_answer = YES if is_prime(guess_num) else NO
     print(f"Question: {guess_num}")
     user_answer = input("Your answer: ")
     return [user_answer, correct_answer]
